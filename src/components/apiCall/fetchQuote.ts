bfb51925b02606beb943ddcc8e3a8fcfeacd5d82
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-07-29 19:12:27 -0400

    Small modification to retrieved data

--- src/components/apiCall/fetchQuote.ts
+++ src/components/apiCall/fetchQuote.ts
@@ -2,8 +2,8 @@ const fetchQuote = async () => {
   const response = await fetch("https://zenquotes.io/api/today");
   const data = await response.json();
 
-  console.log(`data fetched:`, data);
-  return data;
+  console.log(`data fetched:`, data[0].q);
+  return data[0].q;
 };
 
 export default fetchQuote;
