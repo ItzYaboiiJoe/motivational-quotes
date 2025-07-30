412e8002426666793448ae1a4eff43e42e10ce90
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-07-29 21:40:59 -0400

    Created a proxy to fetch the api response and implemented into the main page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import fetchQuote from "@/components/apiCall/fetchQuote";
 
 const Home = () => {
-  fetchQuote();
-  return <div>Home Page</div>;
+  const [quote, setQuote] = useState("Loading...");
+
+  useEffect(() => {
+    const getQuote = async () => {
+      const q = await fetchQuote();
+      setQuote(q);
+    };
+
+    getQuote();
+  }, []);
+
+  return <div>{quote}</div>;
 };
 
 export default Home;
--- src/components/apiCall/fetchQuote.ts
+++ src/components/apiCall/fetchQuote.ts
@@ -1,8 +1,7 @@
 const fetchQuote = async () => {
-  const response = await fetch("https://zenquotes.io/api/today");
+  const response = await fetch("/api/quote");
   const data = await response.json();
-
-  console.log(`data fetched:`, data[0].q);
+  console.log("data fetched:", data[0].q);
   return data[0].q;
 };
 
